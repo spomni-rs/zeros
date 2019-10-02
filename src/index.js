106d61c454a0153c2d220bb7256862efe24b0aed
spomni <mikhail.snitko@gmail.com>
2019-10-02 08:18:01 +0300

    feat: Find solution.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,76 @@
 module.exports = function zeros(expression) {
-  // your solution
+
+  let {twos, fives} = expression
+    .split('*')
+    .map((factor) => {
+      return countFactorsOfTen(factor);
+    })
+    .reduce((counts, {twos, fives}) => {
+      counts.twos += twos;
+      counts.fives += fives;
+      return counts
+    }, {twos: 0, fives: 0});
+    
+  return (twos < fives) ? twos : fives;
+}
+
+function countFactorsOfTen(str){
+
+  const number = parseInt(str);
+  const isDoubleFactorial = str[str.length - 2] === '!';
+
+  if (!isDoubleFactorial){
+    return {
+      twos: countForSingle(2, number),
+      fives: countForSingle(5, number)
+    }
+  } else {
+    return {
+      twos: countForDouble(2, number),
+      fives: countForDouble(5, number)
+    }
+  }
+}
+
+function countForSingle(prime, number){
+  let primeInPow = prime;
+  let count = 0;
+  
+  while (primeInPow <= number){
+    count += (number / primeInPow) | 0;
+    primeInPow *= prime;
+  }
+  
+  return count;
+}
+
+function countForDouble(prime, number){
+
+  let isNumberEven = number % 2 === 0;
+  
+  if (prime === 2 && !isNumberEven){
+    return 0;
+  }
+  
+  if (prime === 2 && isNumberEven){
+    return countForSingle(prime, number);
+  }
+
+  let count = 0;
+  let start = (isNumberEven) ? 2 : 1;
+
+  for (
+    let factor = start;
+    factor <= number;
+    factor += 2
+  ){
+    let subFactor = factor;
+    
+    while (subFactor % prime === 0){
+      count++;
+      subFactor /= prime;
+    }
+  }
+
+  return count;
 }
